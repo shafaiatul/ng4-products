930f3028dc159273e8a0ef2ce8d1ffffc1b18fa0
Shafaiatul Islam <shafaiatul@gmail.com>
2017-05-29 19:21:31 -0400

    Product listing Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Materialize css for ng2
+import { MaterializeModule } from "angular2-materialize";
+
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { ProductFilterPipe } from './products/product-filter.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductFilterPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    //Materilize css for ng2
+    MaterializeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
