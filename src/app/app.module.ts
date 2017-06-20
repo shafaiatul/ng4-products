de212ceafa2203235b8b155d7086c841ee9b0710
Shafaiatul Islam <shafaiatul@gmail.com>
2017-06-19 23:49:28 -0400

    implement router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 // Materialize css for ng2
 import { MaterializeModule } from "angular2-materialize";
 // Components
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { ProductFilterPipe } from './products/pipes/product-filter.pipe';
@@ -20,12 +23,22 @@ import { ProductService } from './products/services/product.service';
     ProductListComponent,
     ProductDetailComponent,
     ProductFilterPipe,
-    StarComponent
+    StarComponent,
+    WelcomeComponent
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot([
+      {path:'products', component: ProductListComponent},
+      {path:'products/:id', component: ProductDetailComponent},
+      {path:'welcome', component: WelcomeComponent},
+      {path:'', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+
+    ]),
     //Materilize css for ng2
     MaterializeModule
   ],
